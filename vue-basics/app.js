ed26474edaa8ea0504216befdbe5e1c3d6379f07
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-05 17:27:11 +0000

    3/5/2023, 11:27 PM

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -1,8 +1,14 @@
-Vue.createApp({
-    data () {
-        return {
-            message: "hohoho"
-        }
-    }
-}).mount("#app")
+const app = Vue.createApp({
+    template: '<h1>I am the template</h1>'
+})
+
+app.mount("#app")
+
+// Vue.createApp({
+//     data () {
+//         return {
+//             message: "hohoho"
+//         }
+//     }
+// }).mount("#app")
 
