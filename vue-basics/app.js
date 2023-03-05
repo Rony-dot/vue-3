21809f0405758df0330bc90d14ab228cdbb17c7b
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-05 20:03:19 +0000

    3/6/2023, 2:03 AM

--- vue-basics/app.js
+++ vue-basics/app.js
@@ -6,7 +6,10 @@ const app = Vue.createApp({
             name: "Rakibul Hassan",
             age: 22,
             showTxt: true,
-            text: ""
+            text: "",
+            x : 0,
+            y : 0,
+            content: ""
         }
     },
     methods:{
@@ -19,8 +22,20 @@ const app = Vue.createApp({
         toggleTxt(){
             this.showTxt = !this.showTxt
         },
-        handleEvent(e){
+        // if no parameters are passed from the HTML, then by default the $event is passed in the function as the first and only parameter.
+        // but if any arguments are passed from the HTML, And at the same time we also want to catch the $event, then we need to explicitly pass $event as the parameter from the HTML.
+        // Also best use is to provide $event as the first parameter of the function, because who knows? if we want to use the same function but without sending any parameter then from the app.js the function will expect a parameter and give error.
+
+        handleEvent(e, data){
            console.log(e, e.type)
+           this.content = e.type
+           if(data){
+            console.log(data)
+           }
+        },
+        handleMouseMove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
